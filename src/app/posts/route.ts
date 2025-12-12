29dcd4f7dcf2ea66bab3c7005b4a055f6a75536c
fatimarezaie256 <fatimarezaie112@gmail.com>
2025-12-12 16:10:17 +0430

    rest api created

--- src/app/posts/route.ts
+++ src/app/posts/route.ts
@@ -1,17 +1,4 @@
-import { data } from "./data";
+import { data } from "./data"; 
 export async function GET(){
-   return Response.json(data);
-}
-export async function POST(request:Request){
-   const body = await request.json(); 
-   const {title,content} = body;
-   const newPost = {
-      id:data.length +1,
-      title,
-      content,
-   }
-   data.push(newPost);
-   return Response.json(newPost,{headers:{"Content-type":"application/json"},
-   status:200,
-   });
-}
+    return await Response.json(data);
+};
