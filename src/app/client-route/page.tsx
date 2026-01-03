4669fab4354a0b0b6f7fe02f80ea28cecae689bc
fatimarezaie256 <fatimarezaie112@gmail.com>
2026-01-03 18:49:52 +0430

    another commits

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,14 +1,26 @@
 "use client";
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Image from "next/image";
 
-import { serverFunction } from "@/utils/secureKey";
-
-function page() {
-    // const result = serverFunction();
-  return (
-    <div>
-        <h1 className='text-3xl p-4 rounded-md shadow-md text-center m-10 bg-amber-700 text-white'>client components</h1>
+export default function ImageSLider(){
+  const settings = {
+    dots:true,
+  };
+  return(
+    <div className="image-slider-container">
+    <Slider {...settings}>
+      <div>
+        <Image src="/images/city.jpg" alt="some thing went wrong" height={1000} width={1000} className="rounded-md mt-42"/></div>
+        <div><Image src="/images/fall.jpg" alt="some thing went wrong" height={1000} width={1000} className="rounded-md mt-42"/></div>
+        <div><Image src="/images/heart.jpg" alt="some thing went wrong" height={1000} width={1000} className="rounded-md mt-42" /></div>
+        <div><Image src="/images/login1.jpg" alt="some thing went wrong" height={1000} width={1000} className="rounded-md mt-42"/></div>
+        <div><Image src="/images/mount.jpg" alt="some thing went wrong" height={1000} width={1000} className="rounded-md mt-42"/></div>
+        <div><Image src="/images/sun.jpg" alt="some thing went wrong" height={1000} width={1000} className="rounded-md mt-42"/></div>
+        <div><img src="/images/1.jpg" alt="" /></div>
+     
+    </Slider>
     </div>
   )
 }
-
-export default page
